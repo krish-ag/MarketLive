d824a2ce0a6ca353b865d8fb143a5acde54927a0
Krish Agrawal <printf.krish@gmail.com>
2024-01-25 00:42:17 +0530

    temporary work on change indicator

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-
-
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {
   Image,
   StatusBar,
@@ -15,25 +13,62 @@ import Bottom from "./components/Bottom.tsx";
 
 function App(): React.JSX.Element {
   const [data, setData] = React.useState({});
-  const getPrice = async () => {
+  const [colors, setColors] = React.useState({});
+  const getPrice = async (pass: any) => {
     const response = await axios.get('http://3.109.185.139/stock');
     if (response.status !== 200) {
       throw new Error('Invalid response status');
     }
-    const data = await response.data;
-    setData(data);
+    const newData = await response.data;
+    setData(newData);
+
+    if (pass && pass.a) {
+      const newColors = {};
+      Object.keys(newData).forEach((category: string) => {
+        Object.keys(newData[category]).forEach((key: string) => {
+          // @ts-ignore
+          newColors[key] = "#000";
+        });
+      });
+      setColors(newColors);
+
+    } else {
+      const newColors = {};
+      Object.keys(newData).forEach((category: string) => {
+        Object.keys(newData[category]).forEach((key: string) => {
+          // @ts-ignore
+          if (newData[category][key].INR > data[category][key].INR) {
+            // @ts-ignore
+            newColors[key] = "#00d600";
+          }
+          // @ts-ignore
+          else if (newData[category][key].INR < data[category][key].INR) {
+            // @ts-ignore
+            newColors[key] = "#ff0000";
+          } else {
+            // @ts-ignore
+            newColors[key] = "#000";
+          }
+        });
+      });
+      if (JSON.stringify(colors) !== JSON.stringify(newColors)) {
+        setColors(newColors);
+      }
+    }
+
   }
 
   useEffect(() => {
-    getPrice();
+    getPrice({a: true});
     const id = setInterval(getPrice, 1000);
     return () => {
       clearInterval(id);
     }
   }, []);
 
-  useEffect(() => {
-  }, [data]);
+// useEffect(() => {
+//   console.log(colors)
+// }, [colors]);
 
 
   const isDarkMode = false;
@@ -43,18 +78,18 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-      />
+      <View style={backgroundStyle}>
+        <StatusBar
+            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            backgroundColor={backgroundStyle.backgroundColor}
+        />
 
-      <Image source={require("./src/VDA.jpg")} style={{resizeMode: "stretch", width: "100%", height: 100}}/>
-      <Graph/>
-      <Cards data={data}/>
-      <Bottom/>
+        <Image source={require("./src/VDA.jpg")} style={{resizeMode: "stretch", width: "100%", height: 100}}/>
+        <Graph/>
+        <Cards data={data} colors={colors}/>
+        <Bottom/>
 
-    </View>
+      </View>
   );
 }
 
